f95787fb0fadb684e4a3c6f2b804ac98d81311ee
Divaksh <divaksh@ubuntu.(none)>
2013-04-23 02:10:30 +0530

    New Docs added

--- MakeSola.sh
+++ MakeSola.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/divaksh/android/sony/sony_kernel/cm-sony-kernel
+cd /home/divaksh/android/sony/sony_kernel/kernel
 export ARCH=arm
 export CROSS_COMPILE=/home/divaksh/android/toolchains/arm-eabi-linaro-4.7/bin/arm-eabi-
 make clean
