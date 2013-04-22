cac3af7fb8bc584de6dcf1e640baa64066152788
Divaksh <divaksh@ubuntu.(none)>
2013-04-23 02:00:36 +0530

    Cleanup and Change Log Update

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 23 */
+/* This file is auto generated, version 24 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#23 SMP PREEMPT Mon Apr 22 22:18:48 IST 2013"
+#define UTS_VERSION "#24 SMP PREEMPT Tue Apr 23 00:56:32 IST 2013"
 #define LINUX_COMPILE_BY "divaksh"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.74-Speedy-5.0"
+#define UTS_RELEASE "3.0.74-Speedy-v5.0+"
